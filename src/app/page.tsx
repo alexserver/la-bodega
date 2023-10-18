867c99238b4f7fb666061fb70e4d14d2a0ea42f2
Alejandro Gomez <alexserverone@gmail.com>
2023-10-18 17:54:39 -0600

    fetching data from supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,26 @@ import ProductList, { type ProductListProps } from '../components/ProductList'
 import { supabase } from '@/lib/supabase'
 
 const getProducts = async () => {
-  const { data, error } = await supabase.from('product').select()
+  const { data, error } = await supabase.from('product').select(`
+    name,
+    description,
+    price,
+    product_image(
+      url
+    )
+  `)
 
   if (error) {
-    throw new Error('Error getting table data')
+    throw new Error(error.message)
   }
 
-  const products = data.map(({ name, description, price }) => ({
+  console.log(data?.at(0)?.product_image)
+
+  const products = data.map(({ name, description, price, product_image }) => ({
     name,
     price,
     description,
-    imageUrl: '',
+    imageUrl: product_image.at(0)?.url ?? '',
   })) as ProductListProps['products']
 
   return { products }
@@ -30,7 +39,7 @@ export default async function Home() {
     <Container size="4" className="min-h-screen flex flex-col justify-stretch">
       <Header />
       <Box py="6">
-        <Heading as="h1" size="7" my="9">
+        <Heading as="h1" size="7" my="2">
           Our Top Products!
         </Heading>
         <ProductList products={products} />
